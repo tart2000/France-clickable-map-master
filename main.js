0efe714d10167d9fd5e599a8d3786579c66ae910
Arthur <tart2000design@gmail.com>
2020-02-12 20:53:42 +0100

    Meilleure nav
    Et plus de productions

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
-for (var i = 0; i < blagues.length; i++) { 
-	var dpt = blagues[i].id; 
-	$('path[name='+dpt+']').attr("class", "dpt done");;
-}
+$( document ).ready(function() {
+	$('#existe').hide(); 
+	$('#afaire').hide();
+
+	for (var i = 0; i < blagues.length; i++) { 
+		var dpt = blagues[i].id; 
+		$('path[name='+dpt+']').attr("class", "dpt done");
+	};
+	counting();
+});
 
 
 $('.dpt').click(function(e) {
 	e.preventDefault();
+	clearAll();
 	dptid = $(this).attr('name');
 	$('#nomDpt').text(dptid);
 	$('#theImg').remove();
 	pic = 'images/travolta.gif';
+	existe = false;
 	// iterate over each element in the array
 	for (var i = 0; i < blagues.length; i++) { 
 	  // look for the entry with a matching `code` value
@@ -20,18 +28,35 @@ $('.dpt').click(function(e) {
 	    pic = blagues[i].pic;
 	    credits = blagues[i].credits;
 
+	    $('#picDpt').prepend('<img id="theImg" class="img-fluid" src='+ pic +' />')
+	    $('#creditsDpt').text('@'+credits);
+	    $('#creditsDpt').attr('href','https://twitter.com/'+credits);
+	    $('#existe').show();
+	    existe = true;
+
 	    // $('#creditsDpt').text(credits);
 	  } 
 	  
+	} // end for 
+
+	if (existe == false) {
+		$('#afaire').show();
 	}
-	$('#picDpt').prepend('<img id="theImg" class="img-fluid" src='+ pic +' />')
+	
 	
 })
-
-counting();
 
 function counting() {
 	dpts = $('.dpt').length; 
 	done = blagues.length;
-	$('#count').text(done+'/'+dpts);
+	$('#progress').attr("aria-valuenow" , done);
+	$("#progress").css("width", dpts/done*100);
 }
+
+function clearAll() {
+	$('#lancement').hide();
+	$('#existe').hide();
+	$('#afaire').hide();
+
+}
+
